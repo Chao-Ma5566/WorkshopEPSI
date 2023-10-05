11b0a4609d84c659ba3a5b16b15c1e9ce243718e
Chao-Ma5566 <mjc1378098@gmail.com>
2023-10-05 09:32:14 +0200

    calendrier

--- Desktop/Workshop/front/src/components/Header.jsx
+++ Desktop/Workshop/front/src/components/Header.jsx
@@ -1,10 +1,7 @@
 import NavBar from "./Navbar"
-import { useContext } from "react"
-import { StoreContext } from "../tools/context.js"
 
 const Header = () => {
-    const [state, dispatch] = useContext(StoreContext)
-
+    
     return(
         <header>
             <NavBar />
--- Desktop/Workshop/front/tailwind.config.js
+++ Desktop/Workshop/front/tailwind.config.js
@@ -1,4 +1,5 @@
 /** @type {import('tailwindcss').Config} */
+
 module.exports = {
   content: [
     "./index.html",
@@ -6,6 +7,28 @@ module.exports = {
   ],
   theme: {
     extend: {},
+    fontFamily : {
+      satoshi: "'Satoshi', sans",
+    },
+    colors : {
+      bleu: {
+       500: "rgb(59 130 246)",
+       800: "rgb(30 64 175)"
+      } ,
+      primary: "#FC5E64",
+      neutral : {
+      50 : "#fafafa",
+      100 : "#f5f5f5",
+      500: "#737373"
+      },
+      green: {
+      500: "#00b0a6",
+      800: "#008290"
+      },
+      yellow: "#e1aa12",
+      black: "rgb(0 0 0)",
+      white: "rgb(255 255 255)"
+     }, 
   },
   plugins: [],
 }
